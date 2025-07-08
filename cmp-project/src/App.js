96b4c60020c5cbd12d3b187c137001dfa2959db6
pratham_tanpure <pratham3778@gmail.com>
2025-07-08 19:31:06 +0530

    basic of styling

--- cmp-project/src/App.js
+++ cmp-project/src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <nav
+        style={{
+          width: "100%",
+          backgroundColor: "red",
+        }}
+      >
+        <ul
+          style={{
+            padding: 0,
+            margin: 0,
+            display: "flex",
+            listStyle: "none",
+            justifyContent: "space-evenly",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <li>
+            <a
+              href="#"
+              style={{
+                textDecoration: "none",
+                color: "white",
+                padding: "8px",
+                display: "block",
+                fontSize: 18,
+              }}
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              style={{
+                textDecoration: "none",
+                color: "white",
+                padding: "8px",
+                display: "block",
+                fontSize: 18,
+              }}
+            >
+              Login
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              style={{
+                textDecoration: "none",
+                color: "white",
+                padding: "8px",
+                display: "block",
+                fontSize: 18,
+              }}
+            >
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <section style={{
+        width: '75%',
+        padding: 34,
+        background: '#f5f5f5',
+        margin: '48px auto'
+      }}>
+        <h1> This is a section </h1>
+      </section>
+
+      <footer style={{
+        background: '#323232',
+        textAlign: 'center',
+        color: 'white',
+        padding: 100
+      }}>
+        <h1> This is footer</h1>
+      </footer>
     </div>
   );
 }
