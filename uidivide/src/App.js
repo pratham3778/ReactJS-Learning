60d95be8456730f23fbdf002655c75214b37872b
pratham_tanpure <pratham3778@gmail.com>
2025-07-09 14:10:05 +0530

    Divide UI learning

---
+++ uidivide/src/App.js
@@ -0,0 +1,13 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Nav></Nav>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
